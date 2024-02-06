59abd35ff28d826abaf85b4838e36d985a82e871
rohitladhar <rohit.ladhar10@gmail.com>
2024-02-06 14:26:51 +0000

    first commit

---
+++ expense_tracker/src/App.vue
@@ -0,0 +1,44 @@
+
+
+<template>
+  
+  <Header/>
+  <div class="container">
+    <Balance :total="total"/>
+    <IncomeExpense/>
+    <TransactionList :transactions="transactions"/>
+    <AddTransaction/>
+  </div>
+</template>
+
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
+  import TransactionList from './components/TransactionList.vue'
+  import AddTransaction from './components/AddTransaction.vue';
+  import {ref , computed} from 'vue';
+  const transactions = ref([
+    {
+        id:1,text:'Flower',amount:-19.99
+    },
+    {
+        id:2,text:'Salary',amount:299.97
+    },
+    {
+        id:3,text:'Book',amount:-10
+    },
+    {
+        id:4,text:'Camera',amount:150
+    },
+  ]);
+
+  const total = computed(()=>{
+    return transactions.value.reduce((acc,transaction)=>{
+      return acc+transaction.amount
+    },0)
+  })
+
+  console.log(total.value)
+</script>
+
---
+++ expense_tracker/src/components/AddTransaction.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Add new Transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter text...">
+        </div>
+        <div class="form-control">
+            <label for="text">Amount
+                <br/>
+                (negative - expense, positive-income)
+            </label>
+            <input type="number" id="amount" placeholder="Enter amount...">
+            <button class="btn"> Add Transaction</button>
+        </div>
+    </form>
+</template>
